b94a430adb06b9f78c47e2bf6b40ed435dfbff40
vans <spentmny@nate.com>
2022-01-01 19:59:47 +0900

    스프링 MVC 개발환경 셋팅

---
+++ src/main/java/com/SpringIOCDIStudy/SpringIocdiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.SpringIOCDIStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocdiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocdiStudyApplication.class, args);
+	}
+
+}
